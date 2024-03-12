240280229f3f6d8369daabb502c835495ab30541
master <13001176374@163.com>
2024-03-12 13:31:41 +0800

    nacos

---
+++ gateway/src/main/java/com/master/configuration/GatewayConfig.java
@@ -0,0 +1,10 @@
+package com.master.configuration;
+
+import com.alibaba.nacos.client.utils.LogUtils;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayConfig {
+}
